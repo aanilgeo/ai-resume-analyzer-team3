4fbe7df6db55b3be955ace73567f409ebc5ceb5c
Jhay Cudia <jc383@njit.edu>
2024-11-23 14:22:00 -0500

    fix: updated package.json dependencies to be testing-compatible

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Home Page', () => {
+  render(
+    <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <App />
+    </MemoryRouter>
+  );
+  const homePageElement = screen.getByText(/Home Page/i);
+  expect(homePageElement).toBeInTheDocument();
 });
