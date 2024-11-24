7837c2587f1a633fc4a067267f48870d68d15164
Jhay Cudia <jc383@njit.edu>
2024-11-24 01:52:34 -0500

    test: Added a test for duplicate registered emails

--- frontend/src/__tests__/Login.test.js
+++ frontend/src/__tests__/Login.test.js
@@ -10,61 +10,60 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate, // Replace useNavigate with the mock function
 }));
 
+test('redirects the user to the homepage and stores token in localStorage after a successful login', async () => {
+    render(
+        <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Login />
+        </MemoryRouter>
+    )
+    const response = { data: { token: 'mockToken' } };
+    axios.post.mockResolvedValue(response); // Mock a successful API response
 
-describe('after a successful login', () => {
-    it('redirects the user to the homepage and stores token in localStorage', async () => {
-        render(
-            <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Login />
-            </MemoryRouter>
-        )
-        const response = { data: { token: 'mockToken' } };
-        axios.post.mockResolvedValue(response); // Mock a successful API response
-        
-        // Change the value of the email input
-        const emailElement = screen.getByLabelText('Email:');
-        fireEvent.change(emailElement, {
-            target: { value: 'valid@email.com' } });
-    
-        // Change the value of the password input
-        const passwordElement = screen.getByLabelText('Password:');
-        fireEvent.change(passwordElement, {
-            target: { value: 'validPassword' } });
-        
-        // Click login
-        const loginButton = screen.getByRole('button', { name: /Login/i });
-        fireEvent.click(loginButton);
-    
-        // Wait to be redirected to home
-        await waitFor(() => {  
-            expect(mockNavigate).toHaveBeenCalledWith('/');
-        })
-        
-        // Check for the token in localStorage
-        expect(localStorage.getItem('token')).toBe('mockToken');
+    // Change the value of the email input
+    const emailElement = screen.getByLabelText('Email:');
+    fireEvent.change(emailElement, {
+        target: { value: 'valid@email.com' }
+    });
+
+    // Change the value of the password input
+    const passwordElement = screen.getByLabelText('Password:');
+    fireEvent.change(passwordElement, {
+        target: { value: 'validPassword' }
+    });
+
+    // Click login
+    const loginButton = screen.getByRole('button', { name: /Login/i });
+    fireEvent.click(loginButton);
+
+    // Wait to be redirected to home
+    await waitFor(() => {
+        expect(mockNavigate).toHaveBeenCalledWith('/');
     })
+
+    // Check for the token in localStorage
+    expect(localStorage.getItem('token')).toBe('mockToken');
 })
 
-describe('after an unsuccessful login', () => {
-    it('shows an "Invalid email or password" error', async () => {
-        render(
-            <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Login />
-            </MemoryRouter>
-        )
-        axios.post.mockRejectedValue(new Error('Invalid login')); // Mock a failed API response
 
-        const emailInput = screen.getByLabelText('Email:');
-        fireEvent.change(emailInput, { value: 'invalid@email.com' });
 
-        const passwordInput = screen.getByLabelText('Password:');
-        fireEvent.change(passwordInput, { value: "invalidPassword" });
+test('shows an "Invalid email or password" error after an unsuccessful login', async () => {
+    render(
+        <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Login />
+        </MemoryRouter>
+    )
+    axios.post.mockRejectedValue(new Error('Invalid login')); // Mock a failed API response
 
-        const loginButton = screen.getByRole('button', { name: /Login/i });
-        fireEvent.click(loginButton);
+    const emailInput = screen.getByLabelText('Email:');
+    fireEvent.change(emailInput, { value: 'invalid@email.com' });
 
-        // Assert that the error message is displayed
-        const error = await screen.findByText('Invalid email or password');
-        expect(error).toBeInTheDocument();
-    })
+    const passwordInput = screen.getByLabelText('Password:');
+    fireEvent.change(passwordInput, { value: "invalidPassword" });
+
+    const loginButton = screen.getByRole('button', { name: /Login/i });
+    fireEvent.click(loginButton);
+
+    // Assert that the error message is displayed
+    const error = await screen.findByText('Invalid email or password');
+    expect(error).toBeInTheDocument();
 })
--- frontend/src/__tests__/Register.test.js
+++ frontend/src/__tests__/Register.test.js
@@ -10,78 +10,75 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate, // Replace useNavigate with the mock function
 }));
 
-describe('when a user registers successfully', () => {
-    it('displays a success message and a "Go Home" button', async () => {
-        render(
-            <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Register />
-            </MemoryRouter>
-        );
-        axios.post.mockResolvedValue(); // Mock successful API response
+test('displays a success message and a "Go Home" button when a user registers successfully', async () => {
+    render(
+        <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Register />
+        </MemoryRouter>
+    );
+    axios.post.mockResolvedValue(); // Mock successful API response
 
-        // input email, username, password, confirm password
-        const emailInput = screen.getByLabelText('Email:');
-        fireEvent.change(emailInput, { value: 'valid@email.com' });
+    // input email, username, password, confirm password
+    const emailInput = screen.getByLabelText('Email:');
+    fireEvent.change(emailInput, { value: 'valid@email.com' });
 
-        const usernameInput = screen.getByLabelText('Username:');
-        fireEvent.change(usernameInput, { value: 'validUsername' });
+    const usernameInput = screen.getByLabelText('Username:');
+    fireEvent.change(usernameInput, { value: 'validUsername' });
 
-        const passwordInput = screen.getByLabelText('Password:');
-        fireEvent.change(passwordInput, { value: 'validPassword' });
+    const passwordInput = screen.getByLabelText('Password:');
+    fireEvent.change(passwordInput, { value: 'validPassword' });
 
-        const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
-        fireEvent.change(confirmPasswordInput, { value: 'validPassword' });
+    const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
+    fireEvent.change(confirmPasswordInput, { value: 'validPassword' });
 
-        const registerButton = screen.getByRole('button', { name: 'Register' });
-        fireEvent.click(registerButton); // Press Register button
+    const registerButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(registerButton); // Press Register button
 
-        // Assert display of success message
-        const successMessage = await screen.findByText(/Registration successful!/i);
-        expect(successMessage).toBeInTheDocument();
+    // Assert display of success message
+    const successMessage = await screen.findByText(/Registration successful!/i);
+    expect(successMessage).toBeInTheDocument();
 
-        // Assert display of 'Go Home' button
-        const goHomeButton = await screen.findByRole('button', { name: /Go Home/i });
-        expect(goHomeButton).toBeInTheDocument();
-    })
-})
+    // Assert display of 'Go Home' button
+    const goHomeButton = await screen.findByRole('button', { name: /Go Home/i });
+    expect(goHomeButton).toBeInTheDocument();
+});
 
-describe("when a user's passwords do not match", () => {
-    it("displays an error message", async () => {
-        render(
-            <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Register />
-            </MemoryRouter>
-        );
 
-        // input email, username, password, confirm password
-        const emailInput = screen.getByLabelText('Email:');
-        fireEvent.change(emailInput, {
-            target: { value: 'valid@example.com' },
-        });
+test("display error message when user's passwords do not match", async () => {
+    render(
+        <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Register />
+        </MemoryRouter>
+    );
 
-        const usernameInput = screen.getByLabelText('Username:');
-        fireEvent.change(usernameInput, {
-            target: { value: 'validUsername' }
-        });
+    // input email, username, password, confirm password
+    const emailInput = screen.getByLabelText('Email:');
+    fireEvent.change(emailInput, {
+        target: { value: 'valid@example.com' },
+    });
 
-        const passwordInput = screen.getByLabelText('Password:');
-        fireEvent.change(passwordInput, {
-            target: { value: 'password' }
-        });
+    const usernameInput = screen.getByLabelText('Username:');
+    fireEvent.change(usernameInput, {
+        target: { value: 'validUsername' }
+    });
 
-        const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
-        fireEvent.change(confirmPasswordInput, {
-            target: { value: 'diffPassword' }
-        });
+    const passwordInput = screen.getByLabelText('Password:');
+    fireEvent.change(passwordInput, {
+        target: { value: 'password' }
+    });
 
-        const registerButton = screen.getByRole('button', { name: 'Register' });
-        fireEvent.click(registerButton); // Press Register button
+    const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
+    fireEvent.change(confirmPasswordInput, {
+        target: { value: 'diffPassword' }
+    });
 
-        // Assert that error message is displayed
-        const errorMessage = await screen.findByText(/Passwords do not match/i);
-        expect(errorMessage).toBeInTheDocument();
-    })
-})
+    const registerButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(registerButton); // Press Register button
+
+    // Assert that error message is displayed
+    const errorMessage = await screen.findByText(/Passwords do not match/i);
+    expect(errorMessage).toBeInTheDocument();
+});
 
 test('displays error message on failed registration', async () => {
     render(
@@ -163,3 +160,49 @@ test('redirects to homepage when "Go Home" button is clicked after successful re
     // Assert that navigation was triggered
     expect(mockNavigate).toHaveBeenCalledWith('/');
 })
+
+test('displays specific error message when registering with duplicate email', async () => {
+    render(
+        <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Register />
+        </MemoryRouter>
+    );
+
+    // Mock the error response from the server for duplicate email
+    axios.post.mockRejectedValue({
+        response: {
+            data: {
+                detail: 'Email already registered'
+            }
+        }
+    });
+
+    const emailInput = screen.getByLabelText('Email:');
+    fireEvent.change(emailInput, { 
+        target: { value: 'invalid@email.com' } 
+    });
+
+    const usernameInput = screen.getByLabelText('Username:');
+    fireEvent.change(usernameInput, { 
+        target: { value: 'username' } 
+    });
+
+    const passwordInput = screen.getByLabelText('Password:');
+    fireEvent.change(passwordInput, { 
+        target: { value: 'password' } 
+    });
+
+    const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
+    fireEvent.change(confirmPasswordInput, { 
+        target: { value: 'password' } 
+    });
+
+    // Submit the form
+    const registerButton = screen.getByRole('button', { name: /Register/i });
+    fireEvent.click(registerButton);
+
+    // Assert display of the error message for duplicate emails
+    await waitFor(() => {
+        expect(screen.getByText('Email already registered')).toBeInTheDocument();
+    });
+});
